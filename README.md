bf188d1cb12c5e853b8d81b7a9d92cdc93d64f97
Olivia Conley <oliviarconley@gmail.com>
2022-08-27 15:15:21 -0400

    Finished HTML, CSS & JS

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Work-Scheduler-080822
+
+Technologies used: HTML, CSS, JavaScript, jQuery, GitHub, VSCode
+## Summary 
+Built simple calendar application using HTML, CSS, Javascript and powered by jQuery that allows the user to save 
+
+## Highlights
+* Uses localStorage in JavaScript to store initials and high scores. 
+* Answer button click triggers correct or wrong answer messaging.  
+
+## Goals 
+
+* Create an JavaScript fundamentals quiz that allows the user to test their knowledge while keeping track of their scores. 
+
+## Learning Experience
+
+* Deepened knowledge of while loops, createElement, appendChild, localStorage and innerHTML. 
+
+## Links
+
+GitHub: https://github.com/oliviaconley
+
+LinkedIn: https://www.linkedin.com/in/olivia-conley/
---
+++ script.js
@@ -0,0 +1,44 @@
+var timeDisplayEl = $('#time-display');
+var currentTime = moment().format('HH');
+console.log(currentTime)
+
+function displayTime() {
+    var rightNow = moment().format('llll');
+    timeDisplayEl.text(rightNow);
+  }
+  setInterval(displayTime, 1000);
+
+  //for when save button is clicked 
+  $(".saveBtn").click(function(event) {
+    // console.log(event.target);
+    // console.log($(event.target).siblings());
+    // console.log($(event.target).siblings("textarea"));
+    console.log($(event.target).siblings("textarea").val());
+    console.log($(event.target).siblings("textarea").attr("id"));
+    var key = $(event.target).siblings("textarea").attr("id");
+    var value = $(event.target).siblings("textarea").val(); 
+    localStorage.setItem(key, value); 
+  });
+
+for (let i = 0; i < $('.container ul li').length; i++) {
+    var element = $('.container ul li textarea')[i]
+    var currentElTime = $(element).attr("rel")
+    
+    if (currentTime > currentElTime) {
+      element.style.backgroundColor = "#d3d3d3";
+   }  else {
+   
+   } if (currentTime === currentElTime) {
+         element.style.backgroundColor = "#ff6961"; 
+   } else {
+   
+   } if (currentTime < currentElTime) {
+      element.style.backgroundColor = "#77dd77"; 
+   
+   }; 
+
+
+     //make conditions for each time, past present and future 
+     //THEN set the class of the element to match the correct color using if/else
+}; 
+
